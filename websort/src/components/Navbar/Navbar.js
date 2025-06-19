628535e2f604edd3e4b9d2f8f607bf4326d492bc
lakshmi863 <sathya.lakshmipathi@gmail.com>
2025-06-20 04:38:42 +0530

    desktop19_change-color

--- websort/src/components/Navbar/Navbar.js
+++ websort/src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="relative p-4 bg-[#DAE5FF] bg-desktop19">
+    <nav className="relative p-4 bg-[#DAE5FF] bg-desktop19 font-['Jacques_Francois']">
       {/* Top Row */}
       <div className="flex items-center justify-between w-full">
         <img
@@ -15,12 +15,13 @@ const Navbar = () => {
           className="h-20 w-52 object-contain"
         />
 
-        <button className="hidden desktop20:inline-block rounded-xl bg-[#212062] text-white w-48 h-12">
+        {/* Contact button - shown in desktop19 and above */}
+        <button className="hidden desktop19:inline-block rounded-xl bg-[#212062] text-white w-48 h-12">
           Contact us
         </button>
 
-        {/* Mobile Menu Icon */}
-        <div className="desktop20:hidden">
+        {/* Menu icon - shown only below desktop19 */}
+        <div className="desktop19:hidden">
           <button onClick={() => setMenuOpen(!menuOpen)}>
             <TbMenu4 size={30} />
           </button>
@@ -28,7 +29,7 @@ const Navbar = () => {
       </div>
 
       {/* Desktop Nav Items */}
-      <ul className="hidden desktop20:flex justify-center flex-wrap space-x-6 text-lg font-medium text-gray-800 mt-4">
+      <ul className="hidden desktop19:flex justify-center flex-wrap space-x-6 text-lg font-medium text-gray-800 mt-4">
         {["About us", "Services", "Projects", "Careers"].map((item, idx) => (
           <li key={idx} className="flex items-center space-x-2">
             <img
@@ -41,13 +42,13 @@ const Navbar = () => {
         ))}
       </ul>
 
-      {/* Mobile Menu - Dropdown */}
+      {/* Mobile Menu Dropdown */}
       {menuOpen && (
         <div
           className={`
             absolute right-4 z-50 mt-4
             w-52 rounded-xl bg-[#1E2755] text-white p-4
-            font-['Jacques_Francois']
+            font-['Jacques_Francois'] menu-box
             iphone56:mt-[5px] iphone57:-mt-[10px] iphone58:mt-[15px]
             iphone61:hidden iphone62:hidden
           `}
@@ -62,28 +63,25 @@ const Navbar = () => {
         </div>
       )}
 
-
-      {/* Bottom Nav - Only on iPhone 61 & 62 */}
-  
-  <div className="hidden iphone61:flex iphone62:flex fixed bottom-0 left-0 w-full z-50 bg-white text-[#1E2755] font-['Jacques_Francois'] py-3 px-6 rounded-t-2xl shadow-md justify-around items-center text-sm">
-  <div className="flex flex-col items-center">
-    <AiOutlineHome className="w-6 h-6" />
-    <span>Home</span>
-  </div>
-  <div className="flex flex-col items-center">
-    <img src="https://i.imghippo.com/files/uf2810vrs.png" alt="services" className="w-6 h-6" />
-    <span>Services</span>
-  </div>
-  <div className="flex flex-col items-center">
-    <img src="https://i.imghippo.com/files/KkXO8020Nk.png" alt="Projects" className="w-6 h-6" />
-    <span>Projects</span>
-  </div>
-  <div className="flex flex-col items-center">
-    <img src="https://i.imghippo.com/files/vNiR2597XDk.png" alt="Careers" className="w-6 h-6" />
-    <span>Careers</span>
-  </div>
-</div>
-      
+      {/* Bottom Nav - Only iPhone 61 & 62 */}
+      <div className="hidden iphone61:flex iphone62:flex fixed bottom-0 left-0 w-full z-50 bg-white text-[#1E2755] py-3 px-6 rounded-t-2xl shadow-md justify-around items-center text-sm">
+        <div className="flex flex-col items-center">
+          <AiOutlineHome className="w-6 h-6" />
+          <span>Home</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src="https://i.imghippo.com/files/uf2810vrs.png" alt="services" className="w-6 h-6" />
+          <span>Services</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src="https://i.imghippo.com/files/KkXO8020Nk.png" alt="Projects" className="w-6 h-6" />
+          <span>Projects</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src="https://i.imghippo.com/files/vNiR2597XDk.png" alt="Careers" className="w-6 h-6" />
+          <span>Careers</span>
+        </div>
+      </div>
     </nav>
   );
 };
